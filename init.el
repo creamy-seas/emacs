c7d6262aa6c366d1460d6bd9a5aabdade7e1a8ff
creamy_seas <ilya.antonov.2013@live.rhul.ac.uk>
2019-02-21 14:28:25 +0000

    latex font highlighting

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-complete-auctex preview-latex cdlatex pyenv-mode elpy jedi htmlize org-reveal flycheck counsel ox-reveal elfeed-web hydra elfeed-goodies elfeed-org try yasnippet-snippets company-irony captain expand-region mark-multiple linum-relative popup-kill-ring swiper epa-file symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete avy ido-vertical-mode beacon f org-bullets spacemacs-theme which-key use-package auctex yasnippet w3 undo-tree switch-window smex ranger powerline nlinum-relative multiple-cursors minimap autopair auto-complete alpha ace-jump-mode)))
+    (exec-path-from-shell fill-column-indicator auto-complete-auctex preview-latex cdlatex pyenv-mode elpy jedi htmlize org-reveal flycheck counsel ox-reveal elfeed-web hydra elfeed-goodies elfeed-org try yasnippet-snippets company-irony captain expand-region mark-multiple linum-relative popup-kill-ring swiper epa-file symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete avy ido-vertical-mode beacon f org-bullets spacemacs-theme which-key use-package auctex yasnippet w3 undo-tree switch-window smex ranger powerline nlinum-relative multiple-cursors minimap autopair auto-complete alpha ace-jump-mode)))
  '(symon-mode nil)
  '(tramp-default-host "192.168.0.5" nil (tramp))
  '(tramp-default-method "ssh" nil (tramp))
@@ -44,5 +44,9 @@
  '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 175 :width normal :foundry "nil" :family "Inconsolata"))))
  '(elfeed-search-date-face ((t :foreground "#11a" :weight bold)))
  '(elfeed-search-feed-face ((t :foreground "#444" :weight bold)))
- '(elfeed-search-title-face ((t :foreground "#3ef" :weight bold))))
+ '(elfeed-search-title-face ((t :foreground "#3ef" :weight bold)))
+ '(font-latex-bold-face ((t (:inherit bold))))
+ '(font-latex-italic-face ((t (:inherit italic))))
+ '(font-latex-math-face ((t (:foreground "#99c616"))))
+ '(font-latex-sedate-face ((t (:foreground "burlywood")))))
 
