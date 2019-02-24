200ccc0e86935eb9de9bde10bf706e1248df9796
creamy_seas <ilya.antonov.2013@live.rhul.ac.uk>
2019-02-24 10:51:43 +0000

    clearance happening

--- ilya_scripts/pdf_engine.sh
+++ ilya_scripts/pdf_engine.sh
@@ -4,27 +4,17 @@
 #----------------------------------------
 # PDF ENGINE
 #----------------------------------------
-# Using the direcotry that BUILD was launched from 
-# 1) determine if we are in the master directory of the latex project
-# 2) source any generated files from the "_output" directory
-# 3) launch latexmk
+# This should be launched with the master file parameters
+# 1) source any previously generated files from the "_output" directory
+# 2) launch latexmk
 #
-# $1 name of the master file (%s in auctex)
+# $1 base name of the master file (%s in auctex)
 #----------------------------------------
 
 
-#1) check if the master file is in this directory
-if [ -f $1 ]; then
-    #2) if the master file is in this directory, all operations done locally
-    relMastDir="."
-else
-    #3) if there is no master file, all operations are done on the parent directory
-    relMastDir="."
-fi
-echo "$pwd"
-#4) copy previous compulation into the master folder
-mv $relMastDir/_output/$1.* $relMastDir/ 2> /dev/null
+#1) copy previous compilation into the master folder
+mv ./_output/$1.* ./ 2> /dev/null
 #mv $relMastDir/_outputs/bibliography* $relMastDir/ 2> /dev/null
 sleep 1
-#5) run latexmk to create a pdf and run in contant update mode (pvc)
+#2) run latexmk to create a pdf and run in contant update mode (pvc)
 latexmk -pdf -pvc $1
