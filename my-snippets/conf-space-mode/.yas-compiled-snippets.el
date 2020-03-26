896a584a988e8b9b6a25de81d8eabe229cd1d1ad
thinkpad chad <ilya.antonov.2013@live.rhul.ac.uk>
2020-03-26 05:01:28 +0000

    auctex configured to run manual install

--- my-snippets/conf-space-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `conf-space-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'conf-space-mode
-		     '(("hs" "########################################\n# ${1:title-here}\n########################################" "hash-title" nil nil nil "/Users/CCCP/creamy_seas/sync_files/emacs_config/personal-snippets/conf-space-mode/ilya-hash-title" nil nil)))
-
-
-;;; Do not edit! File generated at Thu Mar 19 16:34:19 2020
